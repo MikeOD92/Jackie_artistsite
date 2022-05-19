b00fac1d756d3f22878e8bf5ce2406521cf687be
Michael <michaelodell660@gmail.com>
2022-05-19 15:47:18 -0700

    made pages for about CV and contact

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import CV from "./pages/CV";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Header />
         <Routes>
           <Route path={"/"} element={<Home />} />
+          <Route path={"/cv"} element={<CV />} />
+          <Route path={"/about"} element={<About />} />
+          <Route path={"/contact"} element={<Contact />} />
         </Routes>
       </main>
     </div>
---
+++ frontend/src/pages/CV.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const CV: FC = () => {
+  return <div>CV</div>;
+};
+
+export default CV;
