a0b1f1a25b33103bbfda5a56c213efce9e82b9ef
Michael <michaelodell660@gmail.com>
2022-05-23 12:53:18 -0700

    changes to use effect to remove repeted images, added fourth col to img grid

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -12,126 +12,149 @@ const Home: FC = () => {
   const [bucket1, setBucket1] = useState<Array<ArtWork>>([]);
   const [bucket2, setBucket2] = useState<Array<ArtWork>>([]);
   const [bucket3, setBucket3] = useState<Array<ArtWork>>([]);
+  const [bucket4, setBucket4] = useState<Array<ArtWork>>([]);
 
   useEffect(() => {
-    const fetch = async () => {
-      const fetchData = await axios.get("http://localhost:8000/api/artwork");
-      const data = fetchData.data;
-      setArtworks(data);
-    };
-    fetch();
+    if (artworks.length < 1) {
+      const fetch = async () => {
+        const fetchData = await axios.get("http://localhost:8000/api/artwork");
+        const data = fetchData.data;
+        setArtworks(data);
+      };
+      fetch();
+    }
   }, []);
 
   /// this begins the experimental part
   useEffect(() => {
     if (artworks.length === 0) return;
+    setBucket1([]);
+    setBucket2([]);
+    setBucket3([]);
+    setBucket4([]);
     let counter = 1;
     for (let i = 0; i < artworks.length; i++) {
-      if (counter === 4) counter = 1;
-      if (counter === 1) {
-        if (bucket1.length === 0) {
+      if (counter === 5) counter = 1;
+      switch (counter) {
+        case 1:
           setBucket1((bucket1) => [...bucket1, artworks[i]]);
-        } else {
-          setBucket1(bucket1.concat(artworks[i]));
-        }
-        counter++;
-      } else if (counter === 2) {
-        if (bucket2.length === 0) {
+          counter++;
+          break;
+        case 2:
           setBucket2((bucket2) => [...bucket2, artworks[i]]);
-        } else {
-          setBucket2(bucket2.concat(artworks[i]));
-        }
-        counter++;
-      } else if (counter === 3) {
-        if (bucket3.length === 0) {
+          counter++;
+          break;
+        case 3:
           setBucket3((bucket3) => [...bucket3, artworks[i]]);
-        } else {
-          setBucket3(bucket3.concat(artworks[i]));
-        }
-        counter++;
+          counter++;
+          break;
+        case 4:
+          setBucket4((bucket4) => [...bucket4, artworks[i]]);
+          counter++;
+          break;
+        default:
+          return;
       }
     }
   }, [artworks]);
-
   return (
     <>
-      <Container
-        className="mt-3 "
-        style={{
-          marginBottom: "40vh",
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          margin: "0 auto",
-          justifyContent: "center",
-        }}
-      >
-        <Col className="m-1" md={3}>
-          {bucket1
-            ? bucket1.map((work) => {
-                return (
-                  <Link to={`/${work.id}`}>
-                    <Image
-                      className="m-1"
-                      fluid
-                      src={`http://localhost:8000${work.work_img[0].img}`}
-                      alt="art peice"
-                    />
-                  </Link>
-                );
-              })
-            : ""}
-        </Col>
-        <Col className="m-1" md={3}>
-          {bucket2
-            ? bucket2.map((work) => {
-                return (
-                  <Link to={`/${work.id}`}>
-                    <Image
-                      className="m-1"
-                      fluid
-                      src={`http://localhost:8000${work.work_img[0].img}`}
-                      alt="art peice"
-                    />
-                  </Link>
-                );
-              })
-            : ""}
-        </Col>
-        <Col className="m-1" md={3}>
-          {bucket3
-            ? bucket3.map((work) => {
-                return (
-                  <Link to={`/${work.id}`}>
-                    <Image
-                      className="m-1"
-                      fluid
-                      src={`http://localhost:8000${work.work_img[0].img}`}
-                      alt="art peice"
-                    />
-                  </Link>
-                );
-              })
-            : ""}
-        </Col>
-        {/* {artworks.map((work) => {
-            return (
-              <Link to={`/${work.id}`}>
-                <Image
-                  className="m-1"
-                  src={`http://localhost:8000${work.work_img[0].img}`}
-                  style={{
-                    maxHeight: "40vh",
-                    objectFit: "contain",
-                    verticalAlign: "bottom",
-                    display: "inline",
-                  }}
-                  alt="art peice"
-                />
-              </Link>
-            );
-          })} */}
-        {/* </Container> */}
+      <Container className="py-0">
+        <div
+          className="mt-3 "
+          style={{
+            marginBottom: "40vh",
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            margin: "0",
+            // justifyContent: "center",
+          }}
+        >
+          <Col lg={3} md={6} sm={6}>
+            {bucket1
+              ? Array.from(bucket1).map((work) => {
+                  return (
+                    <Link to={`/${work.id}`}>
+                      <Image
+                        style={{ margin: "1px" }}
+                        fluid
+                        src={`http://localhost:8000${work.work_img[0].img}`}
+                        alt="art peice"
+                      />
+                    </Link>
+                  );
+                })
+              : ""}
+          </Col>
+          <Col lg={3} md={6} sm={6}>
+            {bucket2
+              ? bucket2.map((work) => {
+                  return (
+                    <Link to={`/${work.id}`}>
+                      <Image
+                        style={{ margin: "1px" }}
+                        fluid
+                        src={`http://localhost:8000${work.work_img[0].img}`}
+                        alt="art peice"
+                      />
+                    </Link>
+                  );
+                })
+              : ""}
+          </Col>
+          <Col lg={3} md={6} sm={6}>
+            {bucket3
+              ? bucket3.map((work) => {
+                  return (
+                    <Link to={`/${work.id}`}>
+                      <Image
+                        style={{ margin: "1px" }}
+                        fluid
+                        src={`http://localhost:8000${work.work_img[0].img}`}
+                        alt="art peice"
+                      />
+                    </Link>
+                  );
+                })
+              : ""}
+          </Col>
+          <Col lg={3} md={6} sm={6}>
+            {bucket4
+              ? bucket4.map((work) => {
+                  return (
+                    <Link to={`/${work.id}`}>
+                      <Image
+                        style={{ margin: "1px" }}
+                        fluid
+                        src={`http://localhost:8000${work.work_img[0].img}`}
+                        alt="art peice"
+                      />
+                    </Link>
+                  );
+                })
+              : ""}
+          </Col>
+          {/* //////////////////////// */}
+          {/* {artworks.map((work) => {
+          return (
+            <Link to={`/${work.id}`}>
+              <Image
+                className="m-1"
+                src={`http://localhost:8000${work.work_img[0].img}`}
+                style={{
+                  maxHeight: "40vh",
+                  objectFit: "contain",
+                  verticalAlign: "bottom",
+                  display: "inline",
+                }}
+                alt="art peice"
+              />
+            </Link>
+          );
+        })} */}
+          {/* </Container> */}
+        </div>
       </Container>
     </>
   );
