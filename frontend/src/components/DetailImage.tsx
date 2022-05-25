12011329162da6510c1dc1441c6ffdcfa05f26ec
Michael <michaelodell660@gmail.com>
2022-05-25 10:33:31 -0700

    fixing zoom on hover and image positioning on detail page

--- frontend/src/components/DetailImage.tsx
+++ frontend/src/components/DetailImage.tsx
@@ -1,9 +1,9 @@
 import React, { FC, useState, useEffect } from "react";
-import { Image } from "react-bootstrap";
+
 const DetailImage: FC<{ img: string }> = ({ img }) => {
   const [zoom, setZoom] = useState({
-    background: `url('http://localhost:8000${img}')`,
-    backgroundPosition: "0% 0%",
+    backgroundImage: `url('http://localhost:8000${img}')`,
+    backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
     backgroundSize: "contain",
     height: "85vh",
@@ -11,8 +11,8 @@ const DetailImage: FC<{ img: string }> = ({ img }) => {
 
   useEffect(() => {
     setZoom({
-      background: `url('http://localhost:8000${img}')`,
-      backgroundPosition: "0% 0%",
+      backgroundImage: `url('http://localhost:8000${img}')`,
+      backgroundPosition: "center",
       backgroundRepeat: "no-repeat",
       backgroundSize: "contain",
       height: "85vh",
@@ -26,22 +26,30 @@ const DetailImage: FC<{ img: string }> = ({ img }) => {
     const { top, left, width, height } = node.getBoundingClientRect();
     const x = ((event.pageX - left) / width) * 100;
     const y = ((event.pageY - top) / height) * 100;
-
     setZoom({
-      background: `url('http://localhost:8000${img}')`,
+      backgroundImage: `url('http://localhost:8000${img}')`,
       backgroundPosition: `${x}% ${y}%`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "150%",
+      height: "85vh",
+    });
+  };
+  const handleMouseExit = (event: React.MouseEvent<Element, MouseEvent>) => {
+    setZoom({
+      backgroundImage: `url('http://localhost:8000${img}')`,
+      backgroundPosition: "center",
       backgroundRepeat: "no-repeat",
       backgroundSize: "contain",
       height: "85vh",
     });
   };
   return (
-    <div style={zoom} onMouseMove={handleMouseMovement}>
-      <Image
-        fluid
-        src={`http://localhost:8000${img}`}
-        style={{ height: "85vh" }}
-      />
+    <div style={{ margin: "0 auto" }}>
+      <div
+        style={zoom}
+        onMouseMove={handleMouseMovement}
+        onMouseOut={handleMouseExit}
+      ></div>
     </div>
   );
 };
