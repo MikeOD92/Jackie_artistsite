2a42b39d27f9eb3f4ca126019970e4d9014d87e7
Michael <michaelodell660@gmail.com>
2022-05-27 11:01:00 -0700

    styling to align some nav links to the right and to adjust correctly on shrink

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from "react";
 
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Col } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 import { useDispatch } from "react-redux";
 import { logout } from "../store";
 import useAuth from "../hooks/useAuth";
+import { relative } from "path";
 
 const Header: FC = () => {
   // const user = useSelector(selectUser);
@@ -45,48 +46,62 @@ const Header: FC = () => {
       }}
     >
       <Navbar id="navbar" expand="lg" collapseOnSelect>
-        <Nav
-          className="mr-auto"
-          style={{ width: "100%", position: "relative", left: "20px" }}
-        >
-          <LinkContainer to="/">
-            <Nav.Link className="pointer">
-              <strong>Jackie Slanley</strong>
-            </Nav.Link>
-          </LinkContainer>
-
-          <Navbar.Toggle
-            className="pointer"
-            aria-controls="basic-navbar-nav"
-            style={{ position: "absolute", right: "25px" }}
-          />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav>
-              <LinkContainer to="/about">
-                <Nav.Link className="pointer">About</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/cv">
-                <Nav.Link className="pointer">CV</Nav.Link>
-              </LinkContainer>
-              {auth ? (
-                <LinkContainer to="/">
-                  <Nav.Link className="pointer">New</Nav.Link>
+        <Nav className="mr-auto" style={{ width: "100vw" }}>
+          <Col md={9}>
+            <LinkContainer
+              to="/"
+              style={{ position: "relative", left: "20px" }}
+            >
+              <Nav.Link className="pointer">
+                <strong>Jackie Slanley</strong>
+              </Nav.Link>
+            </LinkContainer>
+          </Col>
+          <Col md={3} style={{ display: "flex", flexDirection: "row" }}>
+            <Navbar.Toggle
+              className="pointer"
+              aria-controls="basic-navbar-nav"
+              style={{ position: "absolute", right: "25px", top: "7px" }}
+            />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav
+                style={{
+                  width: "100%",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <LinkContainer to="/about">
+                  <Nav.Link className="pointer rightalign-nav-links">
+                    About
+                  </Nav.Link>
                 </LinkContainer>
-              ) : (
-                ""
-              )}
-              {auth ? (
-                <Nav.Link
-                  className="pointer"
-                  onClick={() => dispatch(logout())}
-                >
-                  Logout
-                </Nav.Link>
-              ) : (
-                ""
-              )}
-            </Nav>
-          </Navbar.Collapse>
+                <LinkContainer to="/cv">
+                  <Nav.Link className="pointer rightalign-nav-links">
+                    CV
+                  </Nav.Link>
+                </LinkContainer>
+                {auth ? (
+                  <LinkContainer to="/">
+                    <Nav.Link className="pointer rightalign-nav-links">
+                      New
+                    </Nav.Link>
+                  </LinkContainer>
+                ) : (
+                  ""
+                )}
+                {auth ? (
+                  <Nav.Link
+                    className="pointer rightalign-nav-links"
+                    onClick={() => dispatch(logout())}
+                  >
+                    Logout
+                  </Nav.Link>
+                ) : (
+                  ""
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </Col>
         </Nav>
       </Navbar>
     </header>
