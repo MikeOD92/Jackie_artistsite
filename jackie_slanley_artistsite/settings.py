e82c76a35bbd079d1af9997342230ca3c6573dca
Michael <michaelodell660@gmail.com>
2022-06-03 19:06:56 -0700

    changed router type

--- jackie_slanley_artistsite/settings.py
+++ jackie_slanley_artistsite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','https://jackie-artist-site-demo.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','jackie-artist-site-demo.herokuapp.com']
 
 
 # Application definition
--- jackie_slanley_artistsite/urls.py
+++ jackie_slanley_artistsite/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -25,7 +25,7 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html")),
+    re_path(r'^(?!(api|admin|media)).*$', TemplateView.as_view(template_name='index.html')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view()),
